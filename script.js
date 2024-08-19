e565138111fda1642cfdd1095b1fdb3b0a7a1504
Ylian1512 <145709984+Ylian1512@users.noreply.github.com>
2024-08-19 17:08:24 +0200

    Upload du développement du site web (build_2)

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        const loading = document.querySelector('#loader')
+        loading.remove();
+    }, 154)
+    
+    
+});
