771a7c1530cf8c1c08b8623978e5c55aca491156
Ylian1512 <145709984+Ylian1512@users.noreply.github.com>
2024-08-20 12:55:12 +0200

    appui pour copier bug pas fix pour linstant

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 154)
     
     document.getElementById('cb_ips').addEventListener('click', () => {
-        new Clipboard().writeText("kaysky.mine.fun");
+        alert("L'appui-pour-copier est indisnonible, nous essayons de régler le problème. S'il vous plaît, copier'le manuellement!")
     });
 });
