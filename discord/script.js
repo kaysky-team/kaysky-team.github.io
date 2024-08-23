898d91fa3cf1bb96e10712c33e3ff12c4d015cde
Ylian1512 <yliansarra002@gmail.com>
2024-08-23 11:38:39 +0200

    BIG CHANGEMENT! le sys de lang pas fait pareil pour paramètres du coup page bientot mais pas fait, page status, page wiki mais pas fini car serveur non ouvert

--- discord/script.js
+++ discord/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const r = document.createElement('meta');
     const id = new URLSearchParams(window.location.search).get('r');
+    new URLSearchParams(window.location.search).delete('r')
+    const r = document.createElement('meta');
     r.httpEquiv = "refresh";
     r.content = `0; url="https://discord.gg/${id}"`;
     document.head.appendChild(r);
